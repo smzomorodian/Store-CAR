f9d7ffda6513f34a0a0b3781d90c3e855a51a4cb
Abolfazli78 <168269289+Abolfazli78@users.noreply.github.com>
2025-04-07 19:49:57 +0330

    AddMapping

---
+++ Application/MappingProfile.cs
@@ -0,0 +1,51 @@
+﻿using AutoMapper;
+using Carproject.Command;
+using Carproject.DTO;
+using Carproject.Model;
+
+namespace Carproject
+{
+
+    public class MappingProfile : Profile
+    {
+        //public MappingProfile()
+        //{
+        //    // نگاشت برای CreateCarDto و معکوس آن به Car
+        //    CreateMap<CommandCar, Car>().ReverseMap();
+
+        //    // نگاشت برای CarDto و معکوس آن به Car
+        //    CreateMap<CarDto, Car>().ReverseMap().ForMember(dest => dest.CategoryName, opt => opt.MapFrom(src => src.Category.Name));
+
+        public MappingProfile()
+        {
+            // نگاشت برای CommandCar به Car
+            CreateMap<CommandCar, Car>()
+                .ForMember(dest => dest.Id, opt => opt.Ignore()) // از آنجا که `Id` خودکار توسط EF پر می‌شود
+                .ForMember(dest => dest.Status, opt => opt.MapFrom(src => src.Status)) // وضعیت خودرو
+                  //.ForMember(dest => dest.Files, opt => opt.MapFrom(src => src.Files.Select(file => new FileBase
+                    //{
+             //    FileName =file.FileName,
+                   // FilePath = file.FilePath
+                   //}).ToList())) // نگاشتفایل‌ها
+                .ForMember(dest => dest.Files, opt => opt.MapFrom(src => src.Files.Select(file =>
+                 new FileBase(file.FileName, file.FilePath)).ToList()))
+
+
+                .ForMember(dest => dest.CategoryId, opt => opt.MapFrom(src => src.CategoryId)); // نگاشت دسته‌بندی
+
+            // نگاشت برای CarDto به Car
+            CreateMap<CarDto, Car>()
+                .ReverseMap() // برای معکوس کردن نگاشت
+                .ForMember(dest => dest.CategoryName, opt => opt.MapFrom(src => src.Category.Name)) // نگاشت نام دسته‌بندی
+                 .ForMember(dest => dest.Status, opt => opt.MapFrom(src => src.Status.ToString()));
+
+            CreateMap<CommanCustomer, Customer>().ReverseMap();  // تبدیل از RegisterCustomerModel به Customer
+            CreateMap<Customer, CustomerDto>().ReverseMap()
+            .ForMember(dest => dest.LoyaltyStatus, opt => opt.MapFrom(src => src.LoyaltyStatus.ToString())); //Enum عدد به رشته 
+            CreateMap<PurchaseHistory, PurchaseHistoryDto>().ReverseMap();
+        }
+
+
+
+    }
+}
