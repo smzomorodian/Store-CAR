e680decc5b2ec29eb8d2ad18a7504a896c5e9f5d
Abolfazli78 <168269289+Abolfazli78@users.noreply.github.com>
2025-04-07 19:23:16 +0330

    AddNotification

---
+++ Domain/Model/Notification.cs
@@ -0,0 +1,44 @@
+﻿namespace Carproject.Model
+{
+    public class Notification
+    {
+        // نوتیف برای اطللاع رسانی خودرو جدید
+        public Notification(string title, string message, DateTime createdAt, int carId, int customerId, Customer customer)
+        {
+            Title = title;
+            Message = message;
+            CreatedAt = createdAt;
+            CarId = carId;
+            CustomerId = customerId;
+            Customer = customer;
+        }
+
+        public Notification(string title, string message, DateTime createdAt, int carId, int customerId)
+        {
+            Title = title;
+            Message = message;
+            CreatedAt = createdAt;
+            CarId = carId;
+            CustomerId = customerId;
+        }
+
+
+        // نوتیف برای فروش جدید 
+        public Notification(string title, string message, DateTime createdAt, int customerId)
+        {
+            Title = title;
+            Message = message;
+            CreatedAt = createdAt;
+            CustomerId = customerId;
+        }
+
+        public int Id { get; set; }               // شناسه نوتیفیکیشن
+        public string Title { get; set; }         // عنوان نوتیفیکیشن
+        public string Message { get; set; }       // متن پیام
+        public DateTime CreatedAt { get; set; }   // زمان ایجاد
+        public bool IsRead { get; set; }          // آیا خوانده شده است؟
+        public Customer Customer { get; set; }            // ارتباط با جدول کاربران
+        public int CarId { get; set; }            // شناسه خودرو
+        public int CustomerId { get; set; }       // شناسه مشتری
+    }
+}
