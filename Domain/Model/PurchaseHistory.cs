29715694c6ed40e44939096393d1343db935038e
Abolfazli78 <168269289+Abolfazli78@users.noreply.github.com>
2025-04-07 19:21:27 +0330

    AddPurchaseHistory

---
+++ Domain/Model/PurchaseHistory.cs
@@ -0,0 +1,19 @@
+﻿namespace Carproject.Model
+{
+    public class PurchaseHistory
+    {
+        public PurchaseHistory(int customerId, DateTime purchaseDate, decimal purchaseAmount, string productName)
+        {
+            CustomerId = customerId;
+            PurchaseDate = purchaseDate;
+            PurchaseAmount = purchaseAmount;
+            ProductName = productName;
+        }
+
+        public int PurchaseHistoryId { get; set; }
+        public int CustomerId { get; set; }
+        public DateTime PurchaseDate { get; set; }
+        public decimal PurchaseAmount { get; set; } // مبلغ خرید
+        public string ProductName { get; set; }
+    }
+}
