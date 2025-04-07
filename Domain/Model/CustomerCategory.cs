8ad3bb5f7723918d5be3f8f4868270a6a6ab1680
Abolfazli78 <168269289+Abolfazli78@users.noreply.github.com>
2025-04-07 19:20:25 +0330

    AddCustomerCategory

---
+++ Domain/Model/CustomerCategory.cs
@@ -0,0 +1,24 @@
+﻿using Domain.Model;
+
+namespace Carproject.Model
+{
+    public class CustomerCategory
+    {
+        public CustomerCategory(int customerId, Customer customer, int categoryId, CarCategory category)
+        {
+            CustomerId = customerId;
+            Customer = customer;
+            CategoryId = categoryId;
+            Category = category;
+        }
+        public CustomerCategory()
+        {
+            
+        }
+        public int CustomerId { get; set; }
+        public Customer Customer { get; set; }
+
+        public int CategoryId { get; set; }
+        public CarCategory Category { get; set; }
+    }
+}
