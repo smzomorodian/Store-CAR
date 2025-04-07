67df29c6e08bdd8ea87d5d1d80cb7bab2a189cf3
Abolfazli78 <168269289+Abolfazli78@users.noreply.github.com>
2025-04-07 19:46:14 +0330

    AddPurchaseHistoryDto

---
+++ Application/DTO/PurchaseHistoryDto.cs
@@ -0,0 +1,11 @@
+﻿
+namespace Carproject.DTO
+{
+    public class PurchaseHistoryDto
+    {
+        public int CustomerId { get; set; }
+        public DateTime PurchaseDate { get; set; }
+        public decimal PurchaseAmount { get; set; }
+        public string ProductName { get; set; }
+    }
+}
