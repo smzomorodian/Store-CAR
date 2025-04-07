117f6ef563d589066e9039c8dbddca4c92697bf4
Abolfazli78 <168269289+Abolfazli78@users.noreply.github.com>
2025-04-07 19:12:28 +0330

    filebasecreate

---
+++ Domain/Model/FileBase.cs
@@ -0,0 +1,22 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Carproject.Model
+{
+    public class FileBase
+    {
+        //public FileBase()
+        //{
+        //}
+
+        public FileBase(string fileName, string filePath)
+        {
+            FileName = fileName;
+            FilePath = filePath;
+        }
+
+        [Key]
+        public int Id { get; set; }
+        public string FileName { get; set; }
+        public string FilePath { get; set; }
+    }
+}
